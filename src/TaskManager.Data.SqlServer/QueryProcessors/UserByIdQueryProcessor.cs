0d3da28ca94ee7d6fcad9cc5d2d8de15588f9fd0
M4C13K <m.smolik@compagnia-lingua.com>
2019-01-12 20:24:55 +0100

    Add user request 50%

---
+++ src/TaskManager.Data.SqlServer/QueryProcessors/UserByIdQueryProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using TaskManager.Data.Entities;
+using TaskManager.Data.QueryProcessors;
+
+namespace TaskManager.Data.SqlServer.QueryProcessors
+{
+	class UserByIdQueryProcessor : IUserByIdQueryProcessor
+	{
+		private readonly ISession _session;
+
+		public UserByIdQueryProcessor(ISession session)
+		{
+			_session = session;
+		}
+
+		public User GetUserById(long userId)
+		{
+			var user = _session.Get<User>(userId);
+			return user;
+		}
+	}
+}
---
+++ src/TaskManager.Web.Api/InquiryProcessing/UserByIdInquiryProcessor.cs
@@ -0,0 +1,18 @@
+﻿using TaskManager.Common.TypeMapping;
+using TaskManager.Data.Exceptions;
+using TaskManager.Data.QueryProcessors;
+using TaskManager.Web.Api.Models;
+
+namespace TaskManager.Web.Api.InquiryProcessing
+{
+	public class UserByIdInquiryProcessor : IUserByIdInquiryProcessor
+	{
+		private readonly IAutoMapper _autoMapper;
+		private readonly ITaskByIdQueryProcessor _queryProcessor;
+
+		public User GetUserById(long userId)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
